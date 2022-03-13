ef98a9b263c197dce620a47cab329fe87ce90262
Shah Axat Devangbhai <axatshah23.it@gmail.com>
2022-03-13 16:59:38 +0530

    Add files via upload
    FAQs added

---
+++ faqs/script.js
@@ -0,0 +1,31 @@
+$(document).on('click',function(){
+	$('.collapse').collapse('hide');
+})
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("TopNavBar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
